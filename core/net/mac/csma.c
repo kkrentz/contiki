209fe9ecd3ed487156cb9937230a4103aab56b73
kkrentz <konrad.krentz@googlemail.com>
2013-05-24 00:21:53 -0700

    llsec: Added a layer in between NETSTACK_MAC and NETSTACK_NETWORK for implementing link layer security

--- core/net/mac/csma.c
+++ core/net/mac/csma.c
@@ -395,7 +395,7 @@ send_packet(mac_callback_t sent, void *ptr)
 static void
 input_packet(void)
 {
-  NETSTACK_NETWORK.input();
+  NETSTACK_LLSEC.input();
 }
 /*---------------------------------------------------------------------------*/
 static int
