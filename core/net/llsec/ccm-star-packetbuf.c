9c954ced40b5d9e1ce079deec19f7a60eb7f9a49
kkrentz <konrad.krentz@gmail.com>
2018-02-12 06:46:29 -0800

    uip: Support for IEEE 802.15.4 short addresses

--- core/net/llsec/ccm-star-packetbuf.c
+++ core/net/llsec/ccm-star-packetbuf.c
@@ -45,44 +45,15 @@
 #include "net/llsec/llsec802154.h"
 #include <string.h>
 
+/*---------------------------------------------------------------------------*/
 #if LLSEC802154_USES_FRAME_COUNTER
-
-/*---------------------------------------------------------------------------*/
-static const uint8_t *
-get_extended_address(const linkaddr_t *addr)
-#if LINKADDR_SIZE == 1
-{
-  /* workaround for short addresses: derive EUI64 as in RFC 6282 */
-  static linkaddr_extended_t template = { { 0x00 , 0x00 , 0x00 ,
-                                            0xFF , 0xFE , 0x00 , 0x00 , 0x00 } };
-
-  template.u8[7] = addr->u8[0];
-
-  return template.u8;
-}
-#elif LINKADDR_SIZE == 2
-{
-  /* workaround for short addresses: derive EUI64 as in RFC 6282 */
-  static linkaddr_extended_t template = { { 0x00 , 0x00 , 0x00 ,
-                                            0xFF , 0xFE , 0x00 , 0x00 , 0x00 } };
-  
-  template.u16[3] = LLSEC802154_HTONS(addr->u16);
-  
-  return template.u8;
-}
-#else /* LINKADDR_SIZE == 2 */
-{
-  return addr->u8;
-}
-#endif /* LINKADDR_SIZE == 2 */
-/*---------------------------------------------------------------------------*/
 void
 ccm_star_packetbuf_set_nonce(uint8_t *nonce, int forward)
 {
   const linkaddr_t *source_addr;
   
   source_addr = forward ? &linkaddr_node_addr : packetbuf_addr(PACKETBUF_ADDR_SENDER);
-  memcpy(nonce, get_extended_address(source_addr), 8);
+  linkaddr_to_eui_64(nonce, source_addr);
   nonce[8] = packetbuf_attr(PACKETBUF_ATTR_FRAME_COUNTER_BYTES_2_3) >> 8;
   nonce[9] = packetbuf_attr(PACKETBUF_ATTR_FRAME_COUNTER_BYTES_2_3) & 0xff;
   nonce[10] = packetbuf_attr(PACKETBUF_ATTR_FRAME_COUNTER_BYTES_0_1) >> 8;
@@ -93,5 +64,5 @@ ccm_star_packetbuf_set_nonce(uint8_t *nonce, int forward)
   nonce[12] = packetbuf_holds_broadcast() ? 0xFF : packetbuf_attr(PACKETBUF_ATTR_NEIGHBOR_INDEX);
 #endif /* LLSEC802154_USES_AUX_HEADER */
 }
-/*---------------------------------------------------------------------------*/
 #endif /* LLSEC802154_USES_FRAME_COUNTER */
+/*---------------------------------------------------------------------------*/
